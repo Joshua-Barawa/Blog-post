4d23b45d4e33683ae06b701336e66a925b3699c9
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-13 10:42:48 +0300

    created views file

--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import InputRequired, Email, ValidationError
 from models import User
 
 
 class RegistrationForm(FlaskForm):
+    profile_img = FileField("Choose a pic", validators=[InputRequired()])
+    full_names = StringField("Enter your names", validators=[InputRequired()])
     email = StringField('Your Email Address', validators=[InputRequired(), Email()])
     username = StringField('Enter your username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
@@ -17,6 +19,6 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Your email address", validators=[InputRequired()])
-    password = PasswordField("Your password:", validators=[InputRequired()])
+    email = StringField("Enter email address", validators=[InputRequired(), Email()])
+    password = PasswordField("Enter password:", validators=[InputRequired()])
     submit = SubmitField("Sign In")
