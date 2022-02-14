2b659cc9159ab29965f0b7ccea9241ee7e5fba9b
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-14 15:20:40 +0300

    deploy to heroku

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
