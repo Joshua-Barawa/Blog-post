55885720409c109399bb4603483f74e14a9426fc
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-14 16:47:27 +0300

    deploy to heroku

--- run.py
+++ run.py
@@ -8,10 +8,10 @@ from flask_script import Manager, Server
 import os
 
 app = Flask(__name__)
-ENV = 'prod'
-app.config['ENV'] = "ENV"
 
-if ENV == 'prod':
+ENV = os.environ.get("ENV")
+
+if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] ="postgres://msfymjupypanwa:017e15df3dbc3b5715be475b57f8400bfef32ee744a18079cf356c92fafedef6@ec2-54-156-110-139.compute-1.amazonaws.com:5432/dfkgqstcrcp5uc"
     app.config['SECRET_KEY'] = "1234567"
